678a565bbc3a67ca6582d98af96595421b2617eb
Hailey Wallace <135855643+hbwalla@users.noreply.github.com>
2024-05-05 13:42:26 -0400

    Update main.py

--- raspberrypi/main.py
+++ raspberrypi/main.py
@@ -2,11 +2,30 @@
 
 class state:
     def __init__(self):
-        self.latent = 0
+        self.launch = 0
         self.accel = 0
-        self.motor_out = 0
-        self.airbrakes = 0
+        self.motorOut = 0
+        self.airBrakes = 0
         self.apogee = 0
         self.main = 0
 
-    def 
+    # where 0 means that the state hasn't been completed, and 1 means 
+    # the state has been completed
+    
+    def rocketAccel(self, acceleration):
+        if acceleration > 32.194:
+            self.accel = 1
+        else:
+            self.accel = self.accel
+
+    def rocketMotorOut(self, acceleration):
+        if acceleration == 32.194:
+            self.motorOut = 1
+        else:
+            self.motorOut = self.motorOut
+        
+    def rocketAirBrakes(self, acceleration, altitude):
+        if altitude > 8000:
+            if acceleration == 32.194:
+                self.airBrakes = 1
+            
