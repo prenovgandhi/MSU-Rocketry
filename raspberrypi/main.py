7c34aa09d15dcee4d6ae27410fe08cb953e9219d
Hailey Wallace <135855643+hbwalla@users.noreply.github.com>
2024-05-05 13:24:27 -0400

    Create main.py

---
+++ raspberrypi/main.py
@@ -0,0 +1,12 @@
+# this is where the PCB project begins!
+
+class state:
+    def __init__(self):
+        self.latent = 0
+        self.accel = 0
+        self.motor_out = 0
+        self.airbrakes = 0
+        self.apogee = 0
+        self.main = 0
+
+    def 
